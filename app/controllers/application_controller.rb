1a342a63d4bf36d602874fd3cef6cbb12649ff0b
jones-py <jones03chimfwembe@gmail.com>
2020-12-10 08:41:10 +0000

    final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  # def current_user
-  #    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  #  end
-
-
-  # def logged_in?
-  #    !!current_user
-  #  end
-
-
   def authenticate_user
     if @current_user == nil
       flash[:notice] = t('notice.login_needed')
